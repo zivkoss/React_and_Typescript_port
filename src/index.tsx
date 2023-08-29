092df0a2fa50487d2f10251bc1db381fdf1a22e8
Nemanja <nemanja@Macs-MacBook-Pro.local>
2023-08-29 18:06:21 +0200

    Project added

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>
+    <textarea></textarea>
+    <div>
+      <button></button>
+    </div>
+  </div>
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
